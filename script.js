d301c6fce06fc962462d2f1461592e3a0f48bcd1
Lucky6400 <94078735+Lucky6400@users.noreply.github.com>
2021-11-23 15:02:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ const mins01 = document.getElementById('mins');
 const seconds01 = document.getElementById('seconds');
 
 
-const newYears = '1 Mar 2022';
+const mahaShiv = '1 Mar 2022';
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
+    const mahaShivDate = new Date(mahaShiv);
     const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+    const totalSeconds = (mahaShivDate - currentDate) / 1000;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const days = Math.floor(totalSeconds / 3600 / 24);
